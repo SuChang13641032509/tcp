b90b472504607ce7a00453e51a15a0c0a752d9d9
SuChang13641032509 <2959124988@qq.com>
2024-06-18 01:26:32 +0800

    first commit

---
+++ src/ReverseServer.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ReverseServer {
+    public static void main(String[] args) throws IOException {
+        int port = 12345; // 默认端口号
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Server started on port " + port);
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            new TCPServerReadThread(clientSocket).start();
+        }
+    }
+}
